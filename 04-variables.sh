79f77a75b8b7f0e34b43cf70961fdd2d78766dc7
Damodharreddy <singamreddy427@gmail.com>
2025-09-04 22:29:21 +0530

    01-hello-world.sh

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hey $PERSON2 how are you?"
+echo "$PERSON2 : Hey $PERSON1, I am good! how about you?"
+echo "$PERSON1 : Am good, what going on?"
+echo "$PERSON2 : Nothing much, leaving to mars to nit, will u join"
+echo "$PERSON1 : Go head will catch up once u come back."
