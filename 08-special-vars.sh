3bba32c0dce5814617687e59ea33f437f1bc962a
Damodharreddy <singamreddy427@gmail.com>
2025-09-05 05:10:42 +0530

    01-hello-world.sh

---
+++ 08-special-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed to script :$@"
+echo "Number of variables :$#"
+echo "Script name :$0"
+echo "Currect directory :$PWD"
+echo "user running this script :$USER"
+echo "Home directory of the user :$HOME"
+echo "PID of the script :$$"
+sleep 10 &
+echo "PID of last command in back ground :$!"
