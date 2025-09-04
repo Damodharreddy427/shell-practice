8087fd34bbba327f0a5bc6a8163d0c20316ecb7f
Damodharreddy <singamreddy427@gmail.com>
2025-09-04 22:11:40 +0530

    01-hello-world.sh

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1 = Trump
+PERSON2 = Musk
+
+echo "$PERSON1 : Hey $PERSON2 how are you?"
+echo "$PERSON2 : Hey $PERSON1, I am good! how about you?"
+echo "$PERSON1 : Am good, what going on?"
+echo "$PERSON2 : Nothing much, leaving to mars to nit, will u join"
+echo "$PERSON1 : Go head will catch up once u come back."
