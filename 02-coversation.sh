a043021cc8aa2d3fa665881e9c99c41255d363df
Damodharreddy <singamreddy427@gmail.com>
2025-09-04 22:02:13 +0530

    01-hello-world.sh

---
+++ 02-coversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump : Hey musk how are you?"
+echo "Musk : I am good! how about you?"
+echo "Trump : Am good, what going on?"
+echo "Musk : Nothing mush, leaving to mars to nit, will u join"
+echo "Trump : Go head will catch up once u come back."
